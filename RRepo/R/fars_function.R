9750cbf61c5dbf2b7f0b2ff33d04078d15888eb6
Aseem Gill <aseem.gill01@gmail.com>
2020-06-15 13:45:21 -0400

    sd

--- RRepo/R/fars_function.R
+++ RRepo/R/fars_function.R
@@ -52,8 +52,7 @@ fars_read <- function(filename) {
 make_filename <- function(year) {
   year <- as.integer(year)
   fp <- sprintf("accident_%d.csv.bz2",year)
-  return(fp)
-  system.file('extdata',fp,package='package name')
+  return(system.file('extdata',fp,package='RRepo'))
 }
 
 
