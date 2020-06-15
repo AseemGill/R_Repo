b44fd9045d952174bd4a4dbc25cac34cf48f4ded
Aseem Gill <aseem.gill01@gmail.com>
2020-06-15 14:19:23 -0400

    as

--- tests/tests.R
+++ tests/tests.R
@@ -1,3 +1,3 @@
 library(testthat)
-devtools::install_github("https://github.com/AseemGill/R_Repo")
-test_that("make_filename",expect_equal(make_filename(2015),"accident_2015.csv.bz2"))
+devtools::install_github("https://github.com/AseemGill/R_Repo",)
+test_that("make_filename",expect_equal(RRepo::make_filename(2015),"accident_2015.csv.bz2",auth_token="c2bf2f93035ee5705a8a108939a0bc5ca7985875"))
